2fd6e3ef867bbe52fa157499d2f5352f8f835441
nouryy17 <dacinour16@gmail.com>
2024-07-18 16:47:23 +0200

    add animtion Shadow

---
+++ Light _ Shadow Text Shadow Animation using Javascript/script.js
@@ -0,0 +1,23 @@
+let text =document.getElementById("text")
+let light =document.getElementById("light")
+
+document.addEventListener("mousemove",function (event) {
+    
+    let mouseX=event.clientX
+    let mouseY=event.clientY
+    light.style.top=mouseY + "px"
+    light.style.left=mouseX + "px"
+
+    let distanceX= mouseX - text.offsetLeft - text.offsetWidth / 2;
+    let distanceY= mouseY - text.offsetTop -  text.offsetHeight / 2;
+    
+    let newShadow="";
+    for (var i = 0; i < 200; i++) {
+        let shadowX = -distanceX * (i / 200)
+        let shadowY = -distanceY * (i / 200)
+        let opactiy = 1 - (i / 100)
+        newShadow += (newShadow ? "," : "") + shadowX 
+        + "px" + shadowY + "px 0 rgba(33,33,33,"+opactiy+")";
+    }
+    text.style.textShadow = newShadow
+})
